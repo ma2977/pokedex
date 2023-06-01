8f4ab9d4a2149980f264ba492066d75778003670
Maricruz Amurrio <mamurrio89@gmail.com>
2023-06-01 13:44:20 -0400

    3rd

--- src/pages/Home/PokemonDetail/PokemonDetail.jsx
+++ src/pages/Home/PokemonDetail/PokemonDetail.jsx
@@ -1,26 +1,41 @@
+import { useState } from "react";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom"
 import { getPokemonById } from "../../../services/getPokemonById";
-
+import "./PokemonDetail.css";
 const PokemonDetail = () => {
-  const {pokemonId}= useParams();
+  const { pokemonId } = useParams();
+  const [pokemon, setPokemon] = useState(null);
 
-useEffect(() => {
+  useEffect(() => {
     const loadPokemon = async () => {
-        const pokemon = await getPokemonById(pokemonId);
-        console.log(pokemon); 
+      const pokemonData = await getPokemonById(pokemonId);
+      setPokemon(pokemonData);
     };
 
     loadPokemon();
-},[]);
+  }, [pokemonId]);
 
   return (
     <div>
-        <h1>Pokemon Details</h1>
-        <p>Here should show up all the pokemon's info</p>
-    <mark>{pokemonId}</mark>
-    </div>
-  )
-}
+<h1 className="pokemon_detail_title">Pokemon Details</h1>
+{pokemon ? (
+<div className="detail_pokemon">
+<p>Here is the information for Pokémon {pokemon.name}</p>
+<img src={pokemon.image} alt={pokemon.name} />
+                    
+        <p>Name: {pokemon.name}</p>
+        {pokemon.types && <p>Types: {pokemon.types.join(", ")}</p>}
+        {pokemon.moves && <p>Moves: {pokemon.moves.join(", ")}</p>}
+        <p>Abilities: {pokemon.abilities.join(" ") }</p>           
 
-export default PokemonDetail
+</div>
+) : (
+<p>Loading...</p>
+)}
+</div>
+);
+};
+export default PokemonDetail;
+
+
